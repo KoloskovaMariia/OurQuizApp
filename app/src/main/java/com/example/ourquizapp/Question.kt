b12bc6e29baa3af40318752a7ed94b3bbbda6b5a
KoloskovaMariia <koloskovamariia@gmail.com>
2021-11-25 17:06:19 +0500

    я настроила кнопку на отслеживание пустоты, задала вопросы и подгрузила картинки к ним

---
+++ app/src/main/java/com/example/ourquizapp/Question.kt
@@ -0,0 +1,12 @@
+package com.example.ourquizapp
+
+data class Question (val id : Int,
+                     val question: String,
+                     val image: Int,
+                     val firstOption: String,
+                     val secondOption: String,
+                     val thirdOption: String,
+                     val fourthOption: String,
+                     val correctAnswer: Int) {
+
+}
