5fe57aee184f355864ca8a6999881047b8c06558
still-at-the-party <darjacazantseva@yandex.ru>
2021-11-27 03:09:32 +0500

    Почти закончена часть с вопросами + кнопками ответа. Нужно дорисовать кнопки. Задники и картинки пока тестовые, на них можно не ругаться

--- app/src/main/java/com/example/ourquizapp/QuestionsActivity.kt
+++ app/src/main/java/com/example/ourquizapp/QuestionsActivity.kt
@@ -8,7 +8,7 @@ class QuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
-
+        
         val questionList = Constants.getQuestions()
         Log.i("Questions size", "${questionList.size}")
     }
