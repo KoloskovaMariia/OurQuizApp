748f68148eb7a5ab76860c5c5932bcefff6e9d88
still-at-the-party <darjacazantseva@yandex.ru>
2021-12-03 02:32:56 +0500

    добавила кнопки, сейчас сделаю так, чтобы они нажимались. Есть ошибки, где их видно. Пока не понимаю, почему они появились. Файл не видит переменные из файла с активити почему-то. Буду чинить.
    Кнопки пока переключаться не умеют

--- app/src/main/java/com/example/ourquizapp/QuestionsActivity.kt
+++ app/src/main/java/com/example/ourquizapp/QuestionsActivity.kt
@@ -5,11 +5,29 @@ import android.os.Bundle
 import android.util.Log
 
 class QuestionsActivity : AppCompatActivity() {
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
         
         val questionList = Constants.getQuestions()
         Log.i("Questions size", "${questionList.size}")
+
+        val currentPosition = 1
+        val question: Question ? = questionList[currentPosition - 1]
+
+        progressBar.progress = currentPosition
+        tv_progress.text = "$currentPosition" + "/" + progressBar.max
+
+        tv_question.text = question!!.question
+        iv_image.setImageResource(question.image)
+        tv_option_one.text = question.firstOption
+        tv_option_two.text = question.secondOption
+        tv_option_three.text = question.thirdOption
+        tv_option_four.text = question.fourthOption
+
     }
 }
