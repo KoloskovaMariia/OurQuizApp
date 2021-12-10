e29b883d8d111753ddbda424cbd3b506109ee5ce
IvanovaAnn <55680355+IvanovaAnn@users.noreply.github.com>
2021-12-10 16:59:22 +0500

    Update ResultActivity.kt

--- app/src/main/java/com/example/ourquizapp/ResultActivity.kt
+++ app/src/main/java/com/example/ourquizapp/ResultActivity.kt
@@ -6,10 +6,13 @@ import com.example.ourquizapp.databinding.ActivityQuestionBinding
 import com.example.ourquizapp.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityResultBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
